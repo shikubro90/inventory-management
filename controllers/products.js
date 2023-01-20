bd0b3a230e87f76b981adcb74ba0a8154a0873ff
Shikubro90 <shikubro90@gmail.com>
2023-01-20 15:59:06 +0600

    update

--- controllers/products.js
+++ controllers/products.js
@@ -1,9 +1,42 @@
-const asyncHandler = require("express-async-handler")
-const Product = require("../modles/product")
+const asyncHandler = require('express-async-handler')
+const Product = require('../modles/product')
 
+// Create products
 
-// Create products 
+const createProducts = asyncHandler(async (req, res) => {
+  const { name, sku, category, price, quantity, description } = req.body
+  // validation
+  if (name || category || quantity || price || description) {
+    res.status(400)
+    throw new Error('Please fill in all fields')
+  }
 
-const createProducts = asyncHandler(async (req, res)=>{
-    
+  // Handle image upload
+  let fileData = {}
+  if (req.file) {
+    fileData = {
+      fileName: req.file.originalname,
+      filePath: req.file.filePath,
+      fileType: req.file.fileType,
+      fileSize: req.file.fileSize,
+    }
+  }
+
+  // create product 
+  const product = await Product.create({
+    user : req.user.id,
+    name,
+    sku, 
+    category,
+    quantity,
+    price,
+    description,
+    image : fileData,
+  });
+
+  res.status(200).json(product)
+
 })
+
+
+module.exports = {createProducts}
--- routes/products.js
+++ routes/products.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const router = express.Router()
 const protect = require("../middleWares/auth")
-const createProducts = require("../controllers/products")
+const {createProducts} = require("../controllers/products")
 const {upload} = require("../utils/fileUpload")
 
 router.post("/",protect, upload.single("image"),createProducts);
