12ed32df3836460d73ff1a4976a2089804ddfc4a
Shikubro90 <shikubro90@gmail.com>
2023-01-26 17:32:59 +0600

    update

--- controllers/contact.js
+++ controllers/contact.js
@@ -1,22 +1,33 @@
-const asyncHandler = require("express-async-handler")
-const User = require("../modles/user")
+const asyncHandler = require('express-async-handler')
+const User = require('../modles/user')
+const sendEmail = require('../utils/sendEmail')
 
-const contact = asyncHandler(async(req,res)=>{
+const contact = asyncHandler(async (req, res) => {
+  const { subject, message } = req.body
+  const user = await User.findById(req.user._id)
 
-    const {subject, message} = req.body;
-    const user = await User.findById(req.user._id)
+  if (!user) {
+    res.status(400)
+    throw new Error('User not found, please signup')
+  }
 
-    if(!user){
-        res.status(400);
-        throw new Error("User not found, please signup")
-    }
+  // validation
+  if (!subject || !message) {
+    res.status(400)
+    throw new Error('Please add subject and message')
+  }
 
-    // validation
-    
+  const send_to = process.env.EMAIL_USER
+  const send_from = process.env.EMAIL_USER
+  const reply_to = user.email
 
-
-
-
+  try {
+    await sendEmaill(subject, message, send_to, send_from, reply_to)
+    res.status(200).json({ success: true, message: 'Email Sent' })
+  } catch (error) {
+    res.status(500)
+    throw new Error('Email not sent, please try again')
+  }
 })
 
-module.exports = {contact}
+module.exports = { contact }
