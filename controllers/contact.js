cf542639ee8d7d7e195a4f4fbc9e31298db1dd4f
Shikubro90 <shikubro90@gmail.com>
2023-01-27 15:45:30 +0600

    update

--- controllers/contact.js
+++ controllers/contact.js
@@ -18,11 +18,11 @@ const contact = asyncHandler(async (req, res) => {
   }
 
   const send_to = process.env.EMAIL_USER
-  const send_from = process.env.EMAIL_USER
+  const sent_from = process.env.EMAIL_USER
   const reply_to = user.email
 
   try {
-    await sendEmaill(subject, message, send_to, send_from, reply_to)
+    await sendEmail(subject, message, send_to, sent_from, reply_to)
     res.status(200).json({ success: true, message: 'Email Sent' })
   } catch (error) {
     res.status(500)
